46388c9ee865f133d4ccbb219a36e132815cbcca
lokhunliao <35357272+lokhunliao@users.noreply.github.com>
2021-03-05 20:17:39 -0800

    Update DirectoryComponent.js

--- nucampsite/src/components/DirectoryComponent.js
+++ nucampsite/src/components/DirectoryComponent.js
@@ -1,37 +1,26 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
-function RenderDirectoryItem({campsite, onClick}) {
+function RenderDirectoryItem({campsite}) {
     return (
-        <Card onClick={() => onClick(campsite.id)}>
-            <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-            <CardImgOverlay>
-                <CardTitle>{campsite.name}</CardTitle>
-            </CardImgOverlay>
+        <Card>
+            <Link to={`/directory/${campsite.id}`}>
+                <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+                <CardImgOverlay>
+                    <CardTitle>{campsite.name}</CardTitle>
+                </CardImgOverlay>
+            </Link>
         </Card>
     );
 }
-function Directory(props) {
 
-    // renderSelectedCampsite(campsite) {
-    //     if (campsite) {
-    //         return (
-    //             <Card>
-    //                 <CardImg top src={campsite.image} alt={campsite.name} />
-    //                 <CardBody>
-    //                     <CardText>{campsite.name}</CardText>
-    //                     <CardText>{campsite.description}</CardText>
-    //                 </CardBody>
-    //             </Card>
-    //         );
-    //     }
-    //     return <div />;
-    // }
+function Directory(props) {
 
     const directory = props.campsites.map(campsite => {
         return (
             <div key={campsite.id} className="col-md-5 m-1">
-                <RenderDirectoryItem campsite={campsite} onClick={props.onClick} />
+                <RenderDirectoryItem campsite={campsite} />
             </div>
         );
     });
@@ -41,11 +30,6 @@ function Directory(props) {
             <div className="row">
                 {directory}
             </div>
-            {/* <div className="row">
-                <div className="col-md-5 m-1">
-                    {this.renderSelectedCampsite(this.state.selectedCampsite)}
-                </div>
-            </div> */}
         </div>
     );
 }
