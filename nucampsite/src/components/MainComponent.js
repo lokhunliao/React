5e883569d7f410f4f713471a516abf560ad186f4
lokhunliao <lokhunnnn@gmail.com>
2021-03-05 17:21:31 -0800

    Adding to the Single Page App

--- nucampsite/src/components/MainComponent.js
+++ nucampsite/src/components/MainComponent.js
@@ -4,14 +4,22 @@ import CampsiteInfo from './CampsiteInfoComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { CAMPSITES } from '../shared/campsites';
+import { COMMENTS } from '../shared/comments';
+import { PARTNERS } from '../shared/partners';
+import { PROMOTIONS } from '../shared/promotions';
 
 class Main extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
             campsites: CAMPSITES,
+            comments: COMMENTS,
+            partners: PARTNERS,
+            promotions: PROMOTIONS
             // selectedCampsite: null
         };
     }
@@ -22,11 +30,17 @@ class Main extends Component {
 
     render() {
 
+        // the reason we use arrow function is because we need to get the data from the parent. 
+        // if we use function declaration, this key word will be undefined. 
         const HomePage = () => {
             return (
-                <Home />
+                <Home
+                    campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
+                    promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
+                    partner={this.state.partners.filter(partner => partner.featured)[0]}
+                />
             );
-        }
+        };
 
         return (
             <div>
@@ -34,6 +48,7 @@ class Main extends Component {
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route extra path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                    <Route extra path='/contactus' component={Contact} />
                     <Redirect to='/home'/>
                 </Switch>
                 {/* <Directory campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)}/> */}
